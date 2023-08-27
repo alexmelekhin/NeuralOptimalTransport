617d0e6cdd0c734b386cb3ae6dc3dc4af7036898
Alexander Melekhin <amelekhin96@gmail.com>
2023-08-27 19:01:57 +0300

    feat: add num_workers argument + removed trailing whitespaces

--- src/tools.py
+++ src/tools.py
@@ -26,7 +26,7 @@ from torchvision.transforms import Compose, Resize, Normalize, ToTensor, RandomC
 from torchvision.datasets import ImageFolder
 
 
-def load_dataset(name, path, img_size=64, batch_size=64, test_ratio=0.1, device='cuda'):
+def load_dataset(name, path, img_size=64, batch_size=64, test_ratio=0.1, device='cuda', num_workers=8):
     if name in ['shoes', 'handbag', 'outdoor', 'church']:
         dataset = h5py_to_dataset(path, img_size)
     elif name in ['celeba_female', 'celeba_male', 'aligned_anime_faces', 'describable_textures']:
@@ -34,7 +34,7 @@ def load_dataset(name, path, img_size=64, batch_size=64, test_ratio=0.1, device=
         dataset = ImageFolder(path, transform=transform)
     else:
         raise Exception('Unknown dataset')
-        
+
     if name in ['celeba_female', 'celeba_male']:
         with open('../datasets/list_attr_celeba.txt', 'r') as f:
             lines = f.readlines()[2:]
@@ -46,13 +46,17 @@ def load_dataset(name, path, img_size=64, batch_size=64, test_ratio=0.1, device=
         idx = np.random.RandomState(seed=0xBADBEEF).permutation(len(dataset))
     else:
         idx = list(range(len(dataset)))
-        
+
     test_size = int(len(idx) * test_ratio)
     train_idx, test_idx = idx[:-test_size], idx[-test_size:]
     train_set, test_set = Subset(dataset, train_idx), Subset(dataset, test_idx)
 
-    train_sampler = LoaderSampler(DataLoader(train_set, shuffle=True, num_workers=8, batch_size=batch_size), device)
-    test_sampler = LoaderSampler(DataLoader(test_set, shuffle=True, num_workers=8, batch_size=batch_size), device)
+    train_sampler = LoaderSampler(
+        DataLoader(train_set, shuffle=True, num_workers=num_workers, batch_size=batch_size), device
+    )
+    test_sampler = LoaderSampler(
+        DataLoader(test_set, shuffle=True, num_workers=num_workers, batch_size=batch_size), device
+    )
     return train_sampler, test_sampler
 
 def ewma(x, span=200):
@@ -61,13 +65,13 @@ def ewma(x, span=200):
 def freeze(model):
     for p in model.parameters():
         p.requires_grad_(False)
-    model.eval()    
-    
+    model.eval()
+
 def unfreeze(model):
     for p in model.parameters():
         p.requires_grad_(True)
     model.train(True)
-    
+
 def weights_init_D(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
@@ -75,7 +79,7 @@ def weights_init_D(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.constant_(m.weight, 1)
         nn.init.constant_(m.bias, 0)
-        
+
 def weights_init_mlp(m):
     classname = m.__class__.__name__
     if classname.find('Linear') != -1:
@@ -89,12 +93,12 @@ def fig2data ( fig ):
     """
     # draw the renderer
     fig.canvas.draw ( )
- 
+
     # Get the RGBA buffer from the figure
     w,h = fig.canvas.get_width_height()
     buf = np.fromstring ( fig.canvas.tostring_argb(), dtype=np.uint8 )
     buf.shape = ( w, h,4 )
- 
+
     # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
     buf = np.roll ( buf, 3, axis = 2 )
     return buf
@@ -114,7 +118,7 @@ def h5py_to_dataset(path, img_size=64):
         data = list(f[a_group_key])
     with torch.no_grad():
         dataset = 2 * (torch.tensor(np.array(data), dtype=torch.float32) / 255.).permute(0, 3, 1, 2) - 1
-        dataset = F.interpolate(dataset, img_size, mode='bilinear')    
+        dataset = F.interpolate(dataset, img_size, mode='bilinear')
 
     return TensorDataset(dataset, torch.zeros(len(dataset)))
 
@@ -123,10 +127,10 @@ def get_loader_stats(loader, batch_size=8, verbose=False):
     block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dims]
     model = InceptionV3([block_idx]).cuda()
     freeze(model)
-    
+
     size = len(loader.dataset)
     pred_arr = []
-    
+
     with torch.no_grad():
         for step, (X, _) in enumerate(loader) if not verbose else tqdm(enumerate(loader)):
             for i in range(0, len(X), batch_size):
@@ -145,10 +149,10 @@ def get_pushed_loader_stats(T, loader, batch_size=8, verbose=False, device='cuda
     block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dims]
     model = InceptionV3([block_idx], use_downloaded_weights=use_downloaded_weights).to(device)
     freeze(model); freeze(T)
-    
+
     size = len(loader.dataset)
     pred_arr = []
-    
+
     with torch.no_grad():
         for step, (X, _) in enumerate(loader) if not verbose else tqdm(enumerate(loader)):
             for i in range(0, len(X), batch_size):
@@ -168,10 +172,10 @@ def get_Z_pushed_loader_stats(T, loader, ZC=1, Z_STD=0.1, batch_size=8, verbose=
     block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dims]
     model = InceptionV3([block_idx], use_downloaded_weights=use_downloaded_weights).to(device)
     freeze(model); freeze(T)
-    
+
     size = len(loader.dataset)
     pred_arr = []
-    
+
     with torch.no_grad():
         for step, (X, _) in enumerate(loader) if not verbose else tqdm(enumerate(loader)):
             Z = torch.randn(len(X), ZC, X.size(2), X.size(3)) * Z_STD
