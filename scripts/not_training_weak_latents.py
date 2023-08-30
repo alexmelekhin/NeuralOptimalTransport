bade6bab2db0b8288ea5080af2dadc88548a7227
Alexander Melekhin <amelekhin96@gmail.com>
2023-08-30 19:43:48 +0300

    fix weak latent training script

--- scripts/not_training_weak_latents.py
+++ scripts/not_training_weak_latents.py
@@ -1,4 +1,5 @@
 import os, sys
+from datetime import datetime
 sys.path.append("..")
 
 import matplotlib
@@ -45,15 +46,15 @@ PngImagePlugin.MAX_TEXT_CHUNK = LARGE_ENOUGH_NUMBER * (1024**2)
 DEVICE_IDS = [0]
 NUM_WORKERS = 8
 
-DATASET1, DATASET1_PATH = 'celeba_female', '/mnt/ssd1/Datasets/celeba_preprocessed'
-DATASET2, DATASET2_PATH = 'aligned_anime_faces', '/mnt/ssd1/Datasets/anime_preprocessed'
+DATASET1, DATASET1_PATH = 'celeba_female', '/gpfs/gpfs0/groznyy.sergey/Datasets/celeba_preprocessed_512'
+DATASET2, DATASET2_PATH = 'aligned_anime_faces', '/gpfs/gpfs0/groznyy.sergey/Datasets/anime_preprocessed_512_v2'
 
 batch_scale = 1  # to try linear scaling rule
 
 T_ITERS = 10
-f_LR, T_LR = 1e-4 * batch_scale, 1e-4 * batch_scale  # baseline is 1e-4
-SRC_IMG_SIZE = 128
-LATENTS_SIZE = 16
+f_LR, T_LR = 1e-4 * np.sqrt(batch_scale), 1e-4 * np.sqrt(batch_scale)  # baseline is 1e-4
+SRC_IMG_SIZE = 512
+LATENTS_SIZE = 64
 
 ZC = 1
 Z_STD = 0.1
@@ -62,17 +63,19 @@ BATCH_SIZE = int(64 * batch_scale)  # baseline is 64
 Z_SIZE = 8
 
 PLOT_INTERVAL = 100
+CKPT_INTERVAL = 100
 COST = 'weak_mse'
-CPKT_INTERVAL = 1000
+FID_INTERVAL = 1000
 MAX_STEPS = 100001
 SEED = 0x000000
 
 # Gamma will linearly increase from 0 to 0.66 during first 25k iters of the potential
 GAMMA0, GAMMA1 = 0.0, 0.66
-GAMMA_ITERS = int(25000 / batch_scale)  # TODO: not sure whether need to scale it
+GAMMA_ITERS = int(25000 / np.sqrt(batch_scale))  # TODO: not sure whether need to scale it
 
 EXP_NAME = f'{DATASET1}_{DATASET2}_T{T_ITERS}_{COST}_{LATENTS_SIZE}_BS{BATCH_SIZE}'
-OUTPUT_PATH = '../checkpoints/{}/{}_{}_{}/'.format(COST, DATASET1, DATASET2, LATENTS_SIZE)
+cur_datetime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+OUTPUT_PATH = '../checkpoints/{}/{}_{}_{}_{}/'.format(COST, DATASET1, DATASET2, LATENTS_SIZE, cur_datetime)
 
 # END OF CONFIG
 ################
@@ -84,7 +87,8 @@ if __name__ == "__main__":
         DATASET2=DATASET2,
         T_ITERS=T_ITERS,
         f_LR=f_LR, T_LR=T_LR,
-        BATCH_SIZE=BATCH_SIZE
+        BATCH_SIZE=BATCH_SIZE,
+        OUTPUT_PATH=OUTPUT_PATH,
     )
 
     assert torch.cuda.is_available()
@@ -111,7 +115,7 @@ if __name__ == "__main__":
     torch.cuda.empty_cache()
     gc.collect()
 
-    f = ResNet_D(size=LATENTS_SIZE, nc=4, nfilter_max=256).cuda()  # lower max filter size just because %)
+    f = ResNet_D(size=LATENTS_SIZE, nc=4).cuda()  # nfilter_max=256 if size=16
     f.apply(weights_init_D)
 
     T = UNet(4+ZC, 4, base_factor=48).cuda() # ZC - noise input channels z
@@ -123,12 +127,12 @@ if __name__ == "__main__":
     if len(DEVICE_IDS) > 1:
         T = nn.DataParallel(T, device_ids=DEVICE_IDS)
         f = nn.DataParallel(f, device_ids=DEVICE_IDS)
-        vae_decoder = nn.DataParallel(vae_decoder, device_ids=DEVICE_IDS)
 
     print('T params:', np.sum([np.prod(p.shape) for p in T.parameters()]))
     print('f params:', np.sum([np.prod(p.shape) for p in f.parameters()]))
 
-    torch.manual_seed(0xBADBEEF); np.random.seed(0xBADBEEF)
+    torch.manual_seed(0xBADBEEF)
+    np.random.seed(0xBADBEEF)
     X_fixed = X_sampler.sample(10)[:,None].repeat(1,4,1,1,1)
     with torch.no_grad():
         Z_fixed = torch.randn(10, 4, ZC, LATENTS_SIZE, LATENTS_SIZE, device='cuda') * Z_STD
@@ -235,7 +239,18 @@ if __name__ == "__main__":
             wandb.log({'Random Test Images' : [wandb.Image(fig2img(fig))]}, step=step)
             plt.close(fig)
 
-        if step % CPKT_INTERVAL == CPKT_INTERVAL - 1:
+        if step % CKPT_INTERVAL == CKPT_INTERVAL - 1:
+            state_dict = {
+                "T_state_dict": T.state_dict(),
+                "f_state_dict": f.state_dict(),
+                "T_opt_state_dict": T_opt.state_dict(),
+                "f_opt_state_dict": f_opt.state_dict(),
+            }
+            ckpt_path = os.path.join(OUTPUT_PATH, f'{SEED}_{step}.pt')
+            torch.save(state_dict, ckpt_path)
+            # wandb.save(ckpt_path, base_path="../")
+
+        if step % FID_INTERVAL == FID_INTERVAL - 1:
             freeze(T)
 
             print('Computing FID')
@@ -243,15 +258,6 @@ if __name__ == "__main__":
             fid = calculate_frechet_distance(mu_data, sigma_data, mu, sigma)
             wandb.log({f'FID (Test)' : fid}, step=step)
             del mu, sigma
-
-            state_dict = {
-                "T_state_dict": T.state_dict(),
-                "f_state_dict": f.state_dict(),
-                "T_opt_state_dict": T_opt.state_dict(),
-                "f_opt_state_dict": f_opt.state_dict(),
-            }
-
-            torch.save(state_dict, os.path.join(OUTPUT_PATH, f'{SEED}_{step}.pt'))
 
         gc.collect()
         torch.cuda.empty_cache()
